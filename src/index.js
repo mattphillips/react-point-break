00749486a60cb15b5f3a15ab2d61b0df61b88826
Matt Phillips <matt@mattphillips.io>
2018-02-07 12:02:36 +0000

    Update name provider export to BreakpointProvider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Consumer from './Consumer';
 import Provider from './Provider';
 
-export { Consumer as Breakpoint, Provider };
+export { Consumer as Breakpoint, Provider as BreakpointProvider };
