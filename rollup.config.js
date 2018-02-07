9a23deef935eba2e57650b55101fe23d06424acc
Matt Phillips <matt@mattphillips.io>
2018-02-07 18:37:28 +0000

    Fix dependency bundling

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
-import pkg from './package.json';
-
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs'
   },
-  external: Object.keys(pkg.dependencies),
+  external: ['react'],
   plugins: [
     commonjs({
       include: 'node_modules/**'
